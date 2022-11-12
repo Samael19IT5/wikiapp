4532318f9d2c51a4fd4041acdc601b09dd0c8e3c
Samael19IT5 <samaelcode@gmail.com>
2022-11-12 20:21:27 +0700

    delete

--- db/migrate/20221111024709_fix_categories.rb
+++
@@ -1,6 +0,0 @@
-class FixCategories < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :categories, :type, :category_type
-    change_column :categories, :category_type, :integer
-  end
-end
