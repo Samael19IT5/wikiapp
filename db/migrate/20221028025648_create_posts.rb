49625dab99922066b4ddbee36eb259c0bd6ed538
Samael19IT5 <samaelcode@gmail.com>
2022-11-12 12:25:47 +0700

    make it better

--- db/migrate/20221028025648_create_posts.rb
+++ db/migrate/20221028025648_create_posts.rb
@@ -3,9 +3,8 @@ class CreatePosts < ActiveRecord::Migration[7.0]
     create_table :posts do |t|
       t.string :title
       t.text :description
-      t.string :kind
       t.string :author
-
+      t.integer :category_id
       t.timestamps
     end
   end
