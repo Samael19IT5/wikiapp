37f706e8806da7a71c383225d293e52662f01d2a
Samael19IT5 <samaelcode@gmail.com>
2022-11-24 10:09:36 +0700

    q1

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :category
+  default_scope -> { where(status: true).order(created_at: :desc) }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_11_031405) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_24_030225) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -26,6 +29,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_11_031405) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id"
+    t.boolean "status", default: true
   end
 
 end
